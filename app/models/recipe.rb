914ab8cdcca3cf08bf3512ecf2d4b4fe0a59b3bc
Luke Burton <lukeburton3d@outlook.com>
2025-05-12 11:34:35 +0100

    validations

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,3 +1,8 @@
 class Recipe < ApplicationRecord
   has_many :bookmarks
+
+  validates :description, presence: true
+  validates :name, uniqueness: true
+  validates :rating, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 5 }
+  
 end
